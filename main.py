40d41deb3654b2c0740b51a2e13929a3cc6f4df8
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-05-15 11:42:44 +0700

    chore(main): change load function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import sleep
+from helper import messages, translate
 from handlers.profiler_menu import ProfilerMenu
 
 class Main:
@@ -5,7 +7,11 @@ class Main:
         self._load()
 
     def _load(self):
+        print(translate.Translate().t("welcome", translate.Translate().t("app_name"), "Python", "Benjamin4k"))
+        print(f"{translate.Translate().t("loading")}...")
+        sleep(2)
         ProfilerMenu.menus()
+
 
 if __name__ == '__main__': Main()
 else: print("This is a module, not a standalone script.")

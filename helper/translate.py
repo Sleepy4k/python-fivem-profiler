07b10579e851f21a9043a2cc1a51fe13c4e7f92b
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-05-15 11:42:11 +0700

    chore(helper): add translate parse and make it more dynamic

--- helper/translate.py
+++ helper/translate.py
@@ -7,14 +7,31 @@ class Translate:
     def __init__(self, lang = None):
         if lang is not None: self._lang = lang
 
-    def get(self, key):
+    def _parse(self, word, *args):
+        for i in range(len(args)):
+            word = word.replace(f"%{i + 1}", args[i])
+
+        return word
+
+    def get(self, key, *args):
         try:
-            return langdata.LANGUAGE[self._lang][key]
+            return self._parse(langdata.LANGUAGE[self._lang][key], *args)
         except KeyError:
             return langconfig.LANGCONFIG["missing"][self._lang].replace("%s", key)
 
-    def translate(self, key):
-        return self.get(key)
+    def translate(self, key, *args):
+        return self.get(key, *args)
 
-    def t(self, key):
-        return self.get(key)
+    def t(self, key, *args):
+        return self.get(key, *args)
+
+    def set_lang(self, lang):
+        if lang in langconfig.LANGCONFIG["available"]:
+            self._lang = lang
+        else:
+            print(f"Language {lang} not available.")
+            exit()
+
+    def get_lang(self):
+        return self._lang
+
